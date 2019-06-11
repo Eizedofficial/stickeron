ee67798c7d868260fbff0bd794780be3074afae7
Eized <brookenlp1@gmail.com>
2019-06-11 10:22:24 +0500

    Пытаюсь

--- site/collection.js
+++ site/collection.js
@@ -1,2 +1,4 @@
 var currentcolor = 1;
 var timer = setInterval(function(){document.getElementById('logo').style.backgroundImage="url('Sources/logo-"+(((++currentcolor)%3)+1)+"_white.svg')"},1000);
+var buyButton = document.getElementById('buy-button');
+buyButton.scrollTo(0,$('iframe').offset().top);
