a0ca30e4af1ba6939cdba117107c678eec23494b
Eized <brookenlp1@gmail.com>
2019-10-05 07:07:48 +0500

    нахуй тут описание, а?

--- site/collection.js
+++ site/collection.js
@@ -44,22 +44,21 @@ $(window).on('load', function(){
     $('#toGrid').removeClass('unactive');
     $('.s_item').hide();
   });
-
-  $(window).resize(function(){if($(window).width()+16>1024){
-    $('aside').hide();
-    $('body').css({'transform': 'translateX(0)', 'overflow' : 'unset', 'overflow-x' : 'hidden'});
-    $('header').css({'transform': 'translateX(0)', 'transition-duration' : '0'});
-  }else{$('aside').show()};fixHeader()})
+var isMenuOpened = false;
+$(window).resize(function(){if($(window).width()+16>1024){
+    if(isMenuOpened){
+      $('aside').hide();
+      $('body').css({'transform': 'translateX(0)', 'overflow' : cartStatus(isCartOpened), 'overflow-x' : 'hidden'});
+      $('header').css({'transform': 'translateX(0)', 'transition-duration' : '0'});
+      isMenuOpened = false;
+    }
+  }else if(isMenuOpened==true){$('aside').show()};fixHeader()})
 
   $('header button').on('click', function(){
     isMenuOpened = true;
     $('body').css({
       "transform" : "translateX(-25%)",
       "overflow": "hidden"
-    });
-    $('header').css({
-      "transform" : "translateX(25%)",
-      "transition" : "transform 0.4s"
     });
     $('aside').css({
       "display" : "block",
@@ -96,8 +95,33 @@ $(window).on('load', function(){
     }
   });
   $('#buyButton').on('click', function(){
+    $('iframe').show();
     $('#newOrder').css('top','0');
-    $('body').css('overflow', 'hidden');
+    $('body').css({
+      'overflow' : 'hidden',
+      'margin-top' : '-50%',
+      'transition' : 'margin-top 0.2s ease-in'
+    });
+    setTimeout(function(){$('#iExit').css('display', 'flex')},200);
     setTimeout(function(){$(window).scrollTop(0)},200);
+    isCartOpened=true;
   });
+  $('#iExit').on('click', function(){
+    $('#iExit').css('display','none');
+    setTimeout(function(){$('iframe').hide()},200);
+    $('#newOrder').css('top', '100%');
+    $('body').css({
+      'overflow-y' : 'scroll',
+      'margin-top' : '0'
+    });
+    isCartOpened=false;
+  });
+var isCartOpened=false;
+function cartStatus(isCartOpened){
+  if(isCartOpened){
+    return 'hidden';
+  }else{
+    return 'unset';
+  }
+}
 });
